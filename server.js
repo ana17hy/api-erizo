339e1be5e7302c9a1d0c28a86839c06441d37f02
ana17hy <ana.huapaya@utec.edu.pe>
2025-09-16 00:25:19 -0500

    fix: port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const db = require("./db");
 
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
